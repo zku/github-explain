ba3e227091d318cdfddfb1e57226da64c343d583
zku <ochmsku@gmail.com>
2025-03-31 10:55:57 +0200

    add comments to main

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
 from agent.agent import CodeAnalysisAgent
-from agent.prompts import add_docstrings
+from agent.prompts import project_analysis
 
 
 async def main():
     """Connects all necessary clients and runs the agent."""
     client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+    # TODO: Clone github project (no submodules, empty LFS filters etc.) in docker container.
+    # TODO: Copy file tree from container to tmp directory (or shared volume mount).
+    # TODO: Replace with default readonly fileserver restricted to a cloned github project.
     async with stdio_client(
         StdioServerParameters(command="uv", args=["run", "./agent/tools/dummy_tool.py"])
     ) as (read, write):
         async with ClientSession(read, write) as session:
             await session.initialize()
             agent = CodeAnalysisAgent(client, [session])
-            await agent.run(add_docstrings.TASK_PROMPT)
+            await agent.run(project_analysis.TASK_PROMPT)
 
 
 if __name__ == "__main__":
